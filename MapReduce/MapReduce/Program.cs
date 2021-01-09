87947f974bc72e843ead1a7ccae4680843eff2d1
NaderHELALI <naderdu44@outlook.fr>
2021-01-09 19:08:14 +0100

    Add Project

---
+++ MapReduce/MapReduce/Program.cs
@@ -0,0 +1,127 @@
+﻿using System.Threading.Tasks;
+using System.Collections.Concurrent;
+using System.Text;
+using System.Threading;
+using System.Collections.Generic;
+using System;
+using System.IO;
+
+
+namespace MapReduce
+{
+    public class Program
+    {
+        public static ConcurrentBag<string> wordBag = new ConcurrentBag<string>();
+        public static BlockingCollection<string> wordChunks = new BlockingCollection<string>(wordBag);
+
+        public static IEnumerable<string> produceWordBlocks(string fileText)
+        {
+            int blockSize = 250;
+            int startPos = 0;
+            int len = 0;
+
+            for (int i = 0; i < fileText.Length; i++)
+            {
+                i = i + blockSize > fileText.Length - 1 ? fileText.Length - 1 : i + blockSize;
+
+                while (i >= startPos && fileText[i] != ' ')
+                {
+                    i--;
+                }
+
+                if (i == startPos)
+                {
+                    i = i + blockSize > (fileText.Length - 1) ? fileText.Length - 1 : i + blockSize;
+                    len = (i - startPos) + 1;
+                }
+                else
+                {
+                    len = i - startPos;
+                }
+
+                yield return fileText.Substring(startPos, len).Trim();
+                startPos = i;
+            }
+        }
+
+
+        public static void mapWords(string fileText)
+        {
+            
+            Parallel.ForEach(produceWordBlocks(fileText), wordBlock =>
+            {   //split the block into words
+                string[] words = wordBlock.Split(' ','\n');
+                StringBuilder wordBuffer = new StringBuilder();
+
+                //cleanup each word and map it
+                foreach (string word in words)
+                {   //Remove all spaces and punctuation
+                    foreach (char c in word)
+                    {
+                        if (char.IsLetterOrDigit(c) || c == '\'' || c == '-')
+                            wordBuffer.Append(c);
+                    }
+                    //Send word to the wordChunks Blocking Collection
+                    if (wordBuffer.Length > 0)
+                    {
+                        wordChunks.Add(wordBuffer.ToString());
+                        wordBuffer.Clear();
+                    }
+                }
+            });
+
+            wordChunks.CompleteAdding();
+        }
+
+        public static ConcurrentDictionary<string, int> wordStore = new ConcurrentDictionary<string, int>();
+
+        public static void reduceWords()
+        {
+            Parallel.ForEach(wordChunks.GetConsumingEnumerable(), word =>
+            {   //if the word exists, use a thread safe delegate to increment the value by 1
+                //otherwise, add the word with a default value of 1
+                wordStore.AddOrUpdate(word, 1, (key, oldValue) => Interlocked.Increment(ref oldValue));
+            });
+        }
+
+        public static void mapReduce(string fileText)
+        {   //Reset the Blocking Collection, if already used
+            if (wordChunks.IsAddingCompleted)
+            {
+                wordBag = new ConcurrentBag<string>();
+                wordChunks = new BlockingCollection<string>(wordBag);
+            }
+
+            //Create background process to map input data to words
+            System.Threading.ThreadPool.QueueUserWorkItem(delegate (object state)
+            {
+                mapWords(fileText);
+            });
+
+            //Reduce mapped words
+            reduceWords();
+            foreach (var temp in wordStore) {
+
+                Console.WriteLine(temp.Key + " : " + temp.Value);
+            }
+            
+        }
+
+
+
+    
+    static void Main(string[] args)
+        {
+            string chemin = @"C:\Users\Asus\Documents\Cours Esilv\A4\S1\Design Pattern\Ex2\MapReduce\mapReduce.txt";
+            string file = File.ReadAllText(chemin);
+
+            Console.WriteLine("------- File ----------");
+            Console.WriteLine(file);
+            Console.WriteLine("\n------- Map Reduce ----------");
+            
+            mapReduce(file);
+        }
+    
+    }
+}
+
---
+++ MapReduce_SansThread/MapReduce_SansThread/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace MapReduce_without_thread
+{
+
+    class Program
+    {
+        //Import the data // Input
+        static StreamReader ImportFile(string path)
+        {
+            StreamReader text = new StreamReader(path);
+            return text;
+        }
+
+        //Splitting and Mapping the data
+        static List<Tuple<string, int>> Mapping(StreamReader file)
+        {
+            List<Tuple<string, int>> mapping = new List<Tuple<string, int>>();
+
+            string ln;
+            while ((ln = file.ReadLine()) != null)
+            {
+                foreach (string word in ln.Split(" "))
+                {
+                    Tuple<string, int> temp = new Tuple<string, int>(word, 1);
+                    mapping.Add(temp);
+                }
+            }
+            return mapping;
+        }
+        //Print the Mapping
+        static void PrintMapping(List<Tuple<string, int>> list)
+        {
+            Console.WriteLine("\n------- Step 2 : MAPPING ----------");
+            foreach (Tuple<string, int> temp in list)
+            {
+
+                Console.WriteLine(temp.Item1 + ", " + temp.Item2);
+            }
+        }
+
+        static Dictionary<string, List<int>> Shuffling(List<Tuple<string, int>> mapping)
+        {
+            Dictionary<string, List<int>> shuffling = new Dictionary<string, List<int>>();
+
+            foreach (Tuple<string, int> temp in mapping)
+            {
+
+                if (shuffling.ContainsKey(temp.Item1) == true)
+                {
+                    shuffling[temp.Item1].Add(temp.Item2);
+
+                }
+                else
+                {
+                    List<int> value = new List<int>();
+                    value.Add(1);
+                    shuffling.Add(temp.Item1, value);
+                }
+            }
+            var l = shuffling.OrderBy(key => key.Key);
+            var dic = l.ToDictionary((keyItem) => keyItem.Key, (valueItem) => valueItem.Value);
+            return dic;
+        }
+        static void PrintShuffling(Dictionary<string, List<int>> shuffling)
+        {
+            Console.WriteLine("\n------- Step 3 : SHUFFLING ----------");
+            foreach (string temp in shuffling.Keys)
+            {
+
+                Console.WriteLine(temp + ", " + printList(shuffling[temp]));
+            }
+        }
+        static string printList(List<int> value)
+        {
+            string res = "( ";
+            foreach (int temp in value)
+            {
+                res += temp.ToString() + " ";
+            }
+            return res + ")";
+        }
+
+
+        static Dictionary<string, int> Reducing(Dictionary<string, List<int>> shuffling)
+        {
+            Dictionary<string, int> reduce = new Dictionary<string, int>();
+            foreach (var temp in shuffling)
+            {
+                reduce.Add(temp.Key, temp.Value.Count);
+            }
+            return reduce;
+        }
+        static void PrintReduce(Dictionary<string, int> reduce)
+        {
+            Console.WriteLine("\n------- Step 4: Reduce ----------");
+            foreach (var temp in reduce)
+            {
+                Console.WriteLine(temp.Key + ", " + temp.Value);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n------- Step 1 : INPUT ----------");
+            StreamReader file = ImportFile(@"C:\Users\Asus\Documents\Cours Esilv\A4\S1\Design Pattern\Ex2\MapReduce\mapReduce.txt");
+            Console.WriteLine("Data has been imported");
+            List<Tuple<string, int>> mapping = Mapping(file);
+            PrintMapping(mapping);
+
+            Dictionary<string, List<int>> shuffling = Shuffling(mapping);
+            PrintShuffling(shuffling);
+
+            Dictionary<string, int> reduce = Reducing(shuffling);
+            PrintReduce(reduce);
+        }
+    }
+}
